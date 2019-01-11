1096cab2cc152e448899dda73e05a5cfa5001a33
Leonardo Vieno <leonardo.vieno@barsanti.edu.it>
2019-01-11 09:10:38 +0100

    11/01/2019 - Nome  e Cognome

--- VienoLeonardo_AngelaGame/VienoLeonardo_AngelaGame.ino
+++ VienoLeonardo_AngelaGame/VienoLeonardo_AngelaGame.ino
@@ -1,9 +1,14 @@
+
+// Vieno Leonardo,ITT Barsanti
+// Started in 11/01/2019 
+// Angela Game : 
+
 void setup() {
-  // put your setup code here, to run once:
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
